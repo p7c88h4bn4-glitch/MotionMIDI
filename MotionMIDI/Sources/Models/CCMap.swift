2919c9710df1f93e9cd3d51d63fb60b673fe57fb
Brad <p7c88h4bn4@privaterelay.appleid.com>
2026-08-26 22:53:38 -0400

    Latest MotionMIDI Updates

---
+++ MotionMIDI/Sources/Models/CCMap.swift
@@ -0,0 +1,8 @@
+//
+//  CCMap.swift
+//  MotionMIDI
+//
+//  Created by Kaya Myers on 8/26/26.
+//
+
+import Foundation
---
+++ MotionMIDI/Sources/Models/CCMapView.swift
@@ -0,0 +1,8 @@
+//
+//  CCMapView.swift
+//  MotionMIDI
+//
+//  Created by Kaya Myers on 8/26/26.
+//
+
+import Foundation
